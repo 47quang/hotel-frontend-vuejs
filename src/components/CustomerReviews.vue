b91d1e752a19b88e12406e0f67af3e11344a903d
Hạ Mỹ <hamylainguyen@gmail.com>
2020-10-23 22:08:06 +0700

    Fix customer review

--- src/components/CustomerReviews.vue
+++ src/components/CustomerReviews.vue
@@ -127,7 +127,6 @@ export default {
       });
     },
     fixReview(id) {
-      console.log(id);
       this.reviewById = this.$store.state.reviewsByCustomer.find((r) => r.id == id);
       this.dialogReviewVisible = true;
     },
